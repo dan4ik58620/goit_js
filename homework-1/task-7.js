78e1df55386d8ed6ceaf3da43988e5ce7e425732
dan4ik58620 <danyakotkot@gmail.com>
2020-03-23 16:07:13 +0200

    task8 is done

--- homework-1/task-7.js
+++ homework-1/task-7.js
@@ -1,37 +1,37 @@
 "use strict";
 
-let country;
-country = prompt("Введите страну!").toLowerCase() ;
+// let country;
+// country = prompt("Введите страну!").toLowerCase() ;
 
-let countryPay;
+// let countryPay;
 
-switch (country) {
-    case "китай":
-        countryPay = 100;
-    alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
-    break;     
+// switch (country) {
+//     case "китай":
+//         countryPay = 100;
+//     alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+//     break;     
 
-    case "чили":
-        countryPay = 250;
-    alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
-    break;
+//     case "чили":
+//         countryPay = 250;
+//     alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+//     break;
 
-    case "австралия":
-        countryPay = 170;
-    alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
-    break;      
+//     case "австралия":
+//         countryPay = 170;
+//     alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+//     break;      
 
-    case "индия":
-        countryPay = 80;
-    alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
-    break;      
+//     case "индия":
+//         countryPay = 80;
+//     alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+//     break;      
 
-    case "ямайка":
-        countryPay = 120;
-    alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
-    break;       
+//     case "ямайка":
+//         countryPay = 120;
+//     alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+//     break;       
     
-    default: 
-    alert(`В вашей стране ${country} доставка не доступна`);
+//     default: 
+//     alert(`В вашей стране ${country} доставка не доступна`);
 
-}
+// }
